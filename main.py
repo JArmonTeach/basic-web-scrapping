758eea4ca91e5397f2c56fb3ed9ee1aaeee5f797
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-01-04 16:43:40 -0500

    debug: add loop to go through divs with same name

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
+html_text = requests.get('https://bible.usccb.org/bible/readings/010423.cfm').text
+soup = BeautifulSoup(html_text, 'lxml')
 
-
+for readings in soup.find_all(class_ = "content-body"):
+    reading = readings.find_next_sibling().text
+    print(reading)
