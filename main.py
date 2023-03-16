2ba60e87a3897679c45d9a1c63db7206faf71b28
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-03-16 01:04:20 -0400

    debug: add scraped txt to slide (format wrong)

--- main.py
+++ main.py
@@ -3,24 +3,18 @@ import requests
 from pptx import Presentation
 from pptx.util import Inches, Pt
 
-# Send a GET requestto the website and parse the HTML content
+# Send a GET request to the website and parse the HTML content
 url = input("Enter the URL of the USCCB website with the readings:")
 html_text = requests.get(url)
 soup = BeautifulSoup(html_text.content, 'html.parser')
 
 # Locate the HTML elements that contain the text you want to scrape
-# scraped_text = []
-# max_chars_per_slide = 520
-# for element in soup.find_all(class_='content-body'):
-#     scraped_text.append(element.get_text())
-text = ''
-for element in soup.find_all(class_='content-body'):
-    text += element.get_text()
+text = soup.find(class_='content-body').get_text()
 
-# Join the elements of the list into a single string
-# text = '\n'.join(scraped_text)
+# Split the text into paragraphs based on <br> tags
+paragraphs = text.split('<br>')
 
-# Create a new PowerPoint presentation, set the size of the slide and add that new slide
+# Create a new PowerPoint presentation, set the size of the slide and add the first slide
 prs = Presentation()
 prs.slide_width = Inches(16)
 prs.slide_height = Inches(9)
@@ -36,34 +30,25 @@ textbox = slide.shapes.add_textbox(left, top, width, height)
 # Reformat the text
 font_name = 'Georgia'
 font_size = Pt(36)
-textbox.text = text
+textbox.text = paragraphs[0].strip()
 font = textbox.text_frame.paragraphs[0].font
 
 # Add the scraped text to slide(s)
-chars_written = 0
-lines_written = 0
-max_chars_per_line = 40
-max_lines_per_slide = 13
-for char in text:
-    textbox.text += char
-    chars_written += 1
-    if chars_written % max_chars_per_line == 0:
-        textbox.text += '\n'
-    if char == '\n' or '<br>':
-        lines_written += 1
-        if lines_written >= max_lines_per_slide:
+line_count = 1
+for paragraph in paragraphs[1:]:
+    # Split each paragraph into lines based on newline characters
+    lines = paragraph.split('\n')
+    for line in lines:
+        # Check if the text in the current slide has exceeded 13 lines
+        if line_count >= 13:
+            # Create a new slide and text box
             slide = prs.slides.add_slide(prs.slide_layouts[6])
-            left = Inches(1)
-            top = Inches(.55)
-            width = Inches(14)
-            height = Inches(7.98)
             textbox = slide.shapes.add_textbox(left, top, width, height)
-            font.name = 'Georgia'
-            font.size = Pt(36)
-            textbox.text = ''
             font = textbox.text_frame.paragraphs[0].font
-            chars_written = 0
-            lines_written = 0
+            line_count = 1
+        # Add the line to the current slide's text box
+        textbox.text_frame.add_paragraph().add_run(line.strip())
+        line_count += 1
 
 # Save the PowerPoint presentation
 prs.save("C:/Users/Colorado/Downloads/Day_Readings.pptx")
